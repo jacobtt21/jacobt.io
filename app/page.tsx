92517197d61619a7be2d79a231431b76d735c8ba
jacobtt21 <jacobt1206@gmail.com>
2023-12-28 11:54:54 -0300

    fixed spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react"
 import Link from "next/link"
 import Image from "next/image"
 import { Poppins } from "next/font/google"
-import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa6";
+import { FaXTwitter, FaGithub, FaLinkedin } from "react-icons/fa6";
 import { IoMoon, IoSunny } from "react-icons/io5";
 
 
@@ -68,10 +68,10 @@ export default function Home() {
   }, [])
 
   return (
-    <main className={`relative min-h-screen justify-center overflow-hidden ${theme === "dark" && "bg-slate-900 text-slate-400"}`}>
+    <main className={`relative min-h-screen justify-center overflow-hidden transition-all ${theme === "dark" && "bg-slate-900 text-slate-400"}`}>
 
       <section className="p-4 w-full z-20">
-        <div className="bg-gradient-to-r from-indigo-500 to-indigo-300 mt-12 mx-auto w-full sm:w-[50%] p-[1px] rounded-lg">
+        <div className="bg-gradient-to-r from-indigo-500 to-indigo-300 mx-auto w-full sm:w-[50%] p-[1px] rounded-lg">
           <div className={`sm:flex items-center rounded-lg text-center sm:justify-between px-4 py-1 ${theme === "dark" ? "bg-slate-800" : "bg-slate-100"}`}>
             <Link aria-label="Link to the homepage of Jacob Thomas" href="/"><code className="text-lg">hi@jacobt.io:~/$</code></Link>
             <div className="mt-4 sm:mt-0 justify-center flex">
@@ -87,7 +87,7 @@ export default function Home() {
                   )}
                 </button>
               </div>
-              <Link aria-label="Twitter link for Jacob Thomas" href="https://twitter.com/0xjacobt" className={`transition rounded p-3 flex items-center gap-2 ${theme === "dark" ? "hover:bg-slate-700" : "hover:bg-slate-200"}`}><FaTwitter /></Link>
+              <Link aria-label="Twitter link for Jacob Thomas" href="https://twitter.com/0xjacobt" className={`transition rounded p-3 flex items-center gap-2 ${theme === "dark" ? "hover:bg-slate-700" : "hover:bg-slate-200"}`}><FaXTwitter /></Link>
               <Link aria-label="Github link for Jacob Thomas" href="https://github.com/jacobtt21" className={`transition rounded p-3 flex items-center gap-2 ${theme === "dark" ? "hover:bg-slate-700" : "hover:bg-slate-200"}`}><FaGithub /></Link>
               <Link aria-label="Linkedin link for Jacob Thomas" href="https://www.linkedin.com/in/jacobt1206/" className={`transition rounded p-3 flex items-center gap-2 ${theme === "dark" ? "hover:bg-slate-700" : "hover:bg-slate-200"}`}><FaLinkedin /></Link>
             </div>
@@ -95,7 +95,7 @@ export default function Home() {
         </div>
       </section>
       
-      <section className="relative mt-10 z-0">
+      <section className="relative z-0">
         <div className="w-full sm:w-[50%] mx-auto p-4">
           <div className="flex items-center gap-5">
             <div className="bg-gradient-to-r from-indigo-500 to-indigo-300 rounded-full h-12 w-12 -z-10"></div>
@@ -110,10 +110,10 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <p className={`text-md mt-8 ${theme === "dark" ? "text-slate-500" : "text-gray-600"}`}>Product enthusiast, creative, and investor based in TX.</p>
+            <p className={`text-md mt-8 ${theme === "dark" ? "text-slate-500" : "text-gray-600"}`}>Product enthusiast, creative, and investor based in Texas.</p>
             <p className={`text-md mt-8 ${theme === "dark" ? "text-slate-500" : "text-gray-600"}`}>I am passionate about technology and design. I believe there is more to come, and I am excited to contribute.</p>
             <p className={`text-md mt-8 text-md mt-10 flex items-center ${theme === "dark" ? "text-slate-500" : "text-gray-600"}`}>
-              Currently working at
+              Currently working on
               <Link aria-label="Link to oustro.xyz" href="https://www.oustro.xyz" className={`ml-1 flex gap-0.5 items-center ${theme === "light" && "text-black"}`}>
                 <Image
                 src="/oustro-logo.svg"
@@ -130,7 +130,7 @@ export default function Home() {
 
       <section aria-label="This section is a carousel of both album covers and movie posters, they are rotating." className="w-full sm:w-[50%] mx-auto">
         <h3 className={`mt-12 border-t pt-4 px-4 text-xl font-semibold ${theme === "dark" ? "border-slate-700" : "border-slate-300"}`}>A Playlist Made by Me</h3>
-        <p className="pt-2 px-4 mx-auto text-sm pb-4">Songs I liked at a certain point, hover to play.</p>
+        <p className={`pt-2 px-4 mx-auto text-sm pb-4 ${theme === "dark" ? "text-slate-500" : "text-gray-600"}`}>Songs I liked at a certain point, hover to play.</p>
         <div className="group w-full inline-flex mt-6 flex-nowrap overflow-hidden sm:[mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
           <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll group-hover:[animation-play-state:paused]" aria-hidden="true">
             {covers.map((cover, index) => (
